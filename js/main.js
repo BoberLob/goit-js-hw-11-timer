1554f6a9609e08156ec7e91df8e4236f0a3957e1
BoberLob <55806787+BoberLob@users.noreply.github.com>
2021-05-27 08:43:59 +0300

    Update main.js
    add click button

--- js/main.js
+++ js/main.js
@@ -10,3 +10,23 @@ const colors = [
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+const refs = {
+    buttonStart: document.querySelector('button[data-action="start"]'),
+    buttonStop: document.querySelector('button[data-action="stop"]'),
+    body: document.querySelector('body'),
+}
+
+refs.buttonStart.addEventListener('click', () => {
+    refs.buttonStart.disabled = true;
+    timerId = setInterval(() => {
+        refs.body.style.backgroundColor =
+            colors[randomIntegerFromInterval(0, colors.length - 1)];
+    }, 1000);
+});
+
+refs.buttonStop.addEventListener('click', () => {
+  refs.buttonStop.disabled = false;
+   refs.buttonStart.disabled = false;
+  clearInterval(null);  
+});
